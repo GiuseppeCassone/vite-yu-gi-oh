6728c68e2273af9428bbded4a75a9e842d21d727
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-03-04 15:46:37 +0100

    Nav completata

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -5,9 +5,30 @@
 </script>
 
 <template>
-    ciao
+    <nav>
+        <img src="/img/yu-gi-oh.png" alt="" id="logo">
+        <h1>Yu-Gi-Oh Api</h1>
+    </nav>
 </template>
 
 <style lang="scss">
+
+    nav{
+        display: flex;
+        align-items: center;
+        width: 100%;
+        height: 100px;
+        background-color: white;
+
+        #logo{
+            height: 100%;
+            padding: 20px 40px;
+        }
+
+        h1{
+            font-size: 44px;
+            font-weight: 400;
+        }
+    }
 
 </style>
